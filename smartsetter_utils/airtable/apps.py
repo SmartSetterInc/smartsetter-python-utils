1ac1104a04f41b75507d7da413eb5d4be448180b
Mohammed Hamdy <mohammed.hamdy777@gmail.com>
2024-11-25 15:51:37 +0000

    Fix airtable app name

--- smartsetter_utils/airtable/apps.py
+++ smartsetter_utils/airtable/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class AirtableUtilsConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
-    name = "airtable_utils"
+    name = "smartsetter_utils.airtable"
