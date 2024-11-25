424b7a22b5c0cc2d1f125ecd5838a7532b8b7d47
Mohammed Hamdy <mohammed.hamdy777@gmail.com>
2024-11-25 09:27:54 +0000

    Add 2 s3 utils

--- smartsetter_utils/aws_utils.py
+++ smartsetter_utils/aws_utils.py
@@ -1,8 +1,25 @@
 import csv
 import tempfile
+import zipfile
+from pathlib import Path
 
 import boto3
 from django.conf import settings
+
+
+def download_s3_file(filename, bucket_name="smartsetter-media", file_suffix=None):
+    temp_download = tempfile.NamedTemporaryFile(suffix=file_suffix)
+    get_boto_client("s3").download_file(bucket_name, filename, temp_download.name)
+    temp_download.seek(0)
+    return temp_download
+
+
+def download_extract_iterate_s3_zipfile(filename, bucket_name="smartsetter-media"):
+    temp_download = download_s3_file(filename, bucket_name, file_suffix=".zip")
+    zip_file = zipfile.ZipFile(open(temp_download.name, "rb"))
+    temp_extract_dir = tempfile.TemporaryDirectory()
+    zip_file.extractall(temp_extract_dir.name)
+    yield from Path(temp_extract_dir.name).iterdir()
 
 
 def read_brand_code_mapping_sheet():
