e376d8bf66a4e0d084e7b05ec5449f86fd5e5068
Mohammed Hamdy <mohammed.hamdy777@gmail.com>
2024-12-17 08:19:51 +0000

    Allow overriding account_sid in get_twilio_client

--- smartsetter_utils/twilio_utils.py
+++ smartsetter_utils/twilio_utils.py
@@ -2,5 +2,5 @@ from django.conf import settings
 from twilio.rest import Client as TwilioClient
 
 
-def get_twilio_client():
-    return TwilioClient(settings.TWILIO_ACCOUNT_SID, settings.TWILIO_AUTH_TOKEN)
+def get_twilio_client(account_sid=settings.TWILIO_ACCOUNT_SID):
+    return TwilioClient(account_sid, settings.TWILIO_AUTH_TOKEN)
