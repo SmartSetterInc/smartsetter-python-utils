f1b69b229fefbe76f5007e29063eef0a3e9d3dc7
Mohammed Hamdy <mohammed.hamdy777@gmail.com>
2024-12-18 11:58:02 +0000

    Allow passing subaccount sid to get_twilio_client

--- smartsetter_utils/twilio_utils.py
+++ smartsetter_utils/twilio_utils.py
@@ -2,5 +2,7 @@ from django.conf import settings
 from twilio.rest import Client as TwilioClient
 
 
-def get_twilio_client(account_sid=settings.TWILIO_ACCOUNT_SID):
-    return TwilioClient(account_sid, settings.TWILIO_AUTH_TOKEN)
+def get_twilio_client(sub_account_sid=None):
+    return TwilioClient(
+        settings.TWILIO_ACCOUNT_SID, settings.TWILIO_AUTH_TOKEN, sub_account_sid
+    )
