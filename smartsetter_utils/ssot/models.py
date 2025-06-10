6f4e4706cd226e8c69f11fe1ab262a3869c450bf
Mohammed Hamdy <mohammed.hamdy777@gmail.com>
2025-06-10 22:01:26 +0000

    Handle missing date

--- smartsetter_utils/ssot/models.py
+++ smartsetter_utils/ssot/models.py
@@ -820,4 +820,6 @@ class Zipcode(TimeStampedModel):
 
 
 def get_hubspot_timestamp_from_iso_date(date: str):
+    if not date:
+        return None
     return int(isodate.parse_datetime(date).timestamp()) * 1000
