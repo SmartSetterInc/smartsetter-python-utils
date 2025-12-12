686319751bb431a3c786f4fc3b05873594bdbcd2
Mohammed Hamdy <mohammed.hamdy777@gmail.com>
2025-12-12 08:18:32 +0000

    Handle Canadian zipcodes when finding zipcode location

--- smartsetter_utils/geo_utils.py
+++ smartsetter_utils/geo_utils.py
@@ -9,7 +9,7 @@ from django.contrib.gis.geos import GEOSGeometry, Point
 
 from smartsetter_utils.elasticsearch import create_elasticsearch_connection
 
-CANADA_ZIPCODE_RE = re.compile(r"[A-Z]\d[A-Z]\s?(\d[A-Z]\d)?$", re.IGNORECASE)
+CANADA_ZIPCODE_RE = re.compile(r"(?P<init>[A-Z]\d[A-Z])\s?(\d[A-Z]\d)?$", re.IGNORECASE)
 USA_ZIPCODE_RE = re.compile(r"\d{5}(-\d{4})?$")
 
 
@@ -36,9 +36,11 @@ def geocode_address(address, zip_code=None):
 
 def query_location_for_zipcode(zip_code):
     attempts = 0
+    if canada_match := CANADA_ZIPCODE_RE.match(zip_code):
+        zip_code = canada_match.group("init")
+    es = create_elasticsearch_connection()
     while attempts < 5:
         try:
-            es = create_elasticsearch_connection()
             zipcode_location_response = es.search(
                 index=settings.ES_ZIPCODE_POLYGONS_INDEX_NAME,
                 body={"query": {"term": {"zip_code": zip_code}}, "size": 1},
