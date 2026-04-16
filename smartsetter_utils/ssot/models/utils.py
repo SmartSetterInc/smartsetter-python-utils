b3883db9c983ddbf7f62048af71c288aac4c2905
Vivek Doshi <vivekd@evincedev.com>
2026-04-16 16:46:13 +0530

    [IMP]Add condition for datetime.

--- smartsetter_utils/ssot/models/utils.py
+++ smartsetter_utils/ssot/models/utils.py
@@ -1,7 +1,10 @@
+from datetime import datetime
 import isodate
 
 
 def get_hubspot_timestamp_from_iso_date(date: str):
     if not date:
         return None
+    if isinstance(date, datetime):
+        return int(date.timestamp()) * 1000
     return int(isodate.parse_datetime(date).timestamp()) * 1000
